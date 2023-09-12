2c4e80c3af970594b905843095de705ed4bd036f
lazycaterpillar <absolem.jk@gmail.com>
2023-09-12 18:58:00 +0900

    image added

--- post/models.py
+++ post/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Post(models.Model):
+    image = models.ImageField(upload_to='images/', blank=True)
     country = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     content = models.TextField()
--- post/urls.py
+++ post/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path
 from post import views
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('<int:post_id>/', views.detail),
     path('<int:post_id>/delete/', views.delete),
     path('<int:post_id>/update/', views.update),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
