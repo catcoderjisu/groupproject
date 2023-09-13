0eb5526c92721594462ea8591d67540888c7d037
JaeYoonAI <minamljy@gmail.com>
2023-09-13 18:52:23 +0900

    conflict solved

--- post/views.py
+++ post/views.py
@@ -1,6 +1,8 @@
 
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from post.models import Post
+
 
 # Create your views here.
 
@@ -19,12 +21,12 @@ def create(request):
     if request.method == 'GET':
         return render(request, 'post/create.html')
     elif request.method == 'POST':
-        # Post 생성로직
+        image = request.FILES.get('image')
         country = request.POST['country']
         city = request.POST['city']
         content = request.POST['content']
         Post.objects.create(
-            country=country, city=city, content=content, user=request.user
+            image=image, country=country, city=city, content=content, user=request.user
         )
         return redirect('/post/')
 
@@ -36,12 +38,35 @@ def detail(request, post_id):
             'post': post,
         }
         return render(request, 'post/detail.html', context)
-    pass
 
 
-def update(request):
-    pass
+def update(request, post_id):
+    if request.method == 'GET':
+        post = Post.objects.get(id=post_id)
+        context = {
+            'post': post,
+        }
+        return render(request, 'post/update.html', context)
+    elif request.method == 'POST':
+        image = request.FILES.get('image')
+        post = Post.objects.get(id=post_id)
+        country = request.POST['country']
+        city = request.POST['city']
+        content = request.POST['content']
+        is_completed = request.POST.get('is_completed', False)
+        post.country = country
+        post.city = city
+        post.content = content
+        post.is_completed = is_completed
+        post.save()
+        return redirect('/post/')
 
 
-def delete(request):
-    pass
+def delete(request, post_id):
+    if request.method == 'POST':
+        post = Post.objects.get(id=post_id)
+        if request.user == post.user:
+            post.delete()
+            return redirect('/post/')
+        else:
+            return HttpResponse('권한이 없습니다.')
