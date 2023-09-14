919dcdc5c424dc3b77d80eb159de90ed45a42bf2
lazycaterpillar <absolem.jk@gmail.com>
2023-09-14 17:05:30 +0900

    create에 날짜 추가

--- post/models.py
+++ post/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django_countries.fields import CountryField
 
 # Create your models here.
 
 
 class Post(models.Model):
     image = models.ImageField(upload_to='images/', blank=True)
-    country = models.CharField(max_length=100)
+    country = CountryField(blank_label="(select country)")
     city = models.CharField(max_length=100)
     content = models.TextField()
+    starting_date = models.DateField('starting date',null=True, blank=True)
+    finishing_date = models.DateField('finishing date',null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     is_completed = models.BooleanField(default=False)
